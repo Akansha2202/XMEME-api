0a23b2e10e4b28eaf0020f81c23e968991901e51
Akansha2202 <mini.akansha007@gmail.com>
2021-02-10 21:26:13 +0530

    added sh files

---
+++ server_run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# file : /scripts/cdjava
+#
+
+python manage.py runserver 8081
+
---
+++ sleep.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# start time
+date +"%H:%M:%S"
+
+# sleep for 10 seconds
+sleep 10
+
+# end time
+date +"%H:%M:%S"
