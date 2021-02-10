660737fd786dfc71587a0bc87e437e29ddd3c581
Akansha2202 <mini.akansha007@gmail.com>
2021-02-11 00:53:18 +0530

    changes

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #apt-get update  # To get the latest package lists
-pip3 install -U -r requirements.txt
+pip3 install -r requirements.txt
 
 #conda create -n djangoenv
 #conda activate djangoenv
-cd /home/akansha/crio/mini-akansha007-me_buildout_xmeme/Backend/XMEME
-python manage.py makemigrations
-python manage.py migrate
-python manage.py makemigrations
+# cd /home/akansha/crio/mini-akansha007-me_buildout_xmeme/Backend/XMEME
+# python manage.py makemigrations
+# python manage.py migrate
+# python manage.py makemigrations
 #etc.
